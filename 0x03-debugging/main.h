6262b2e9890cac785a44a7f62fcf3ec9567cd688
Tomiwa Oladipo <tomiwaoladipo1@gmail.com>
2022-09-19 01:58:22 -0700

    done

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,34 +5,15 @@
 
 void positive_or_negative(int i);
 
-/**
-* largest_number - returns the largest of 3 numbers
-* @a: first integer
-* @b: second integer
-* @c: third integer
-* Return: largest number
-*/
 
-int largest_number(int a, int b, int c)
-{
-	int largest;
 
-	if (a >= b && b >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && a >= c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
 
-	return (largest);
-}
 
+
+
+
+
+int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
