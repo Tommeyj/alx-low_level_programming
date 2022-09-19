d4c3d4f85b71c35a195ecea685363a20c166b0de
Tomiwa Oladipo <tomiwaoladipo1@gmail.com>
2022-09-19 01:44:45 -0700

    done

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,26 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && b >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && a >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
+
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
