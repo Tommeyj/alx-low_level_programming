4b1bc37e968d996b5ed6f029582c95249bf951e3
Tomiwa Oladipo <tomiwaoladipo1@gmail.com>
2022-09-21 01:46:51 -0700

    Add: 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* file: main.h
+* Desc: header file containing prototypes for all functions used in 0x05-pointers_arrays_strings
+*
+*/
+
+#include <stdlib.h>
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
