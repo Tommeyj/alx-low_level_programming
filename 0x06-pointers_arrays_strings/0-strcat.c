de8245475442b10c6cb80362f7d0c18eea6e1063
Tomiwa Oladipo <tomiwaoladipo1@gmail.com>
2022-09-22 02:00:11 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,18 +2,13 @@
 
 /**
 * *_strcat - catenatte @src to @dest
-*
+* main - check code
 * @dest: an input string
 * @src: an input string
 * Return: A pointer to the resulting string
 *
 */
-char *dest;
-char *src;
 
-int main()
-{
-	char *_strcat(char *dest, char *src);
-	
-	return (dest);
-}
+
+char *_strcat(char *dest, char *src);
+return (dest);
