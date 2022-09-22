d8ffe43fc34fbb821c53bf979bb466d48d40ae5f
Tomiwa Oladipo <tomiwaoladipo1@gmail.com>
2022-09-21 23:37:06 -0700

    test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,12 +13,13 @@ char *_strcat(char *dest, char *src)
 {
 	char *temp = dest;
 
-	while (*dest)
-		dest++;
-
-	while (*src)
-		*dest++ = *src++;
-
-		*dest = '\0';
+	while (*dest[length] != '\0')
+	{++length;
+	}
+	for (j = 0; *src[j] != '\0'; ++j, ++length)
+	{
+		*dest[length] = *src[j];
+	}
+	*dest[length] = '\0';
 	return (temp);
 }
